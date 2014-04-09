58b83ad1eb4eb90df794f3b8f77ad430031c379d
Ningxin Hu <ningxin.hu@intel.com>
2014-04-09 10:13:16 +0800

    Add Chrome Apps support for depth video example.

---
+++ video_depth/background.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function createWindow(url) {
+  console.log('loading ' + url);
+  chrome.app.window.create(url, {
+    width: 1024,
+    height: 480,
+  });
+}
+
+function onLaunched(launchData) {
+  createWindow('index.html');
+}
+
+chrome.app.runtime.onLaunched.addListener(onLaunched);
